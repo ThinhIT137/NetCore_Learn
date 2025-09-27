660b6f07372a13f476a4972ace6aede87d4c0b4e
ThinhIT137 <tranthinh130720052@gmail.com>
2025-09-27 20:52:04 +0700

    thuc hanh 03

---
+++ Day06/Controllers/StudentController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Day06.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        private static readonly List<Student> ListStudents = new List<Student>();
+
+        public IActionResult Index()
+        {
+            return View(ListStudents);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>(){
+                new SelectListItem{ Text = "IT", Value = "1"},
+                new SelectListItem{ Text = "BE", Value = "2"},
+                new SelectListItem{ Text = "CE", Value = "3"},
+                new SelectListItem{ Text = "EE", Value = "4"},
+            };
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student s)
+        {
+            if (ModelState.IsValid)
+            {
+                if (ListStudents.Any())
+                {
+                    s.Id = ListStudents.Last<Student>().Id + 1;
+                }
+                else s.Id = 0;
+                ListStudents.Add(s);
+                return View("Index", ListStudents);
+            }
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+            {
+                new SelectListItem{ Text = "IT", Value = "1"},
+                new SelectListItem{ Text = "BE", Value = "2"},
+                new SelectListItem{ Text = "CE", Value = "3"},
+                new SelectListItem{ Text = "EE", Value = "4"},
+            };
+            return View(s);
+        }
+    }
+}
---
+++ Day06/Models/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Student
+{
+    [Key]
+    public int Id { get; set; } // Mã sinh viên
+
+    [Required(ErrorMessage = "Họ tên bắt buộc phải nhập")]
+    [RegularExpression(@"^[\p{L}\s]{4,100}$", ErrorMessage = "Tên tối đa 4 ký tự tối thiểu 100")]
+    public string Name { get; set; } // Họ tên
+
+    [Required(ErrorMessage = "Email bắt buộc phải nhập")]
+    [RegularExpression(@"[A-Za-z0-9._%+-]+@[Gg][Mm][Aa][Ii][Ll].[Cc][Oo][Mm]",
+        ErrorMessage = "Email không hợp lệ")]
+    public string Email { get; set; } // Email
+
+    [Required(ErrorMessage = "Mật khẩu bắt buộc phải nhập")]
+    [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$", ErrorMessage = "Mật khẩu gồm chữ thường chữ hoa số và kí tự đặc biệt tối thiểu là 8 ký tự")]
+    //[StringLength(100, MinimumLength = 8, ErrorMessage = "Mật khẩu tối thiểu 8 ký tự")]
+    public string Password { get; set; } // Mật khẩu
+
+    [Required(ErrorMessage = "Ngành học bắt buộc phải chọn")]
+    public string? Branch { get; set; } // Ngành học (enum)
+
+    [Required(ErrorMessage = "Giới tính bắt buộc phải chọn")]
+    public Gender? Gender { get; set; } // Giới tính (enum)
+
+    public bool IsRegular { get; set; } // Hệ: true = chính quy, false = phi chính quy
+
+    [Required(ErrorMessage = "Địa chỉ bắt buộc phải nhập")]
+    [DataType(DataType.MultilineText)]
+    public string Address { get; set; } // Địa chỉ
+
+    [Required(ErrorMessage = "Ngày sinh bắt buộc phải nhập")]
+    [Range(typeof(DateTime), "1/1/1963", "12/31/2005",
+        ErrorMessage = "Ngày sinh phải từ 01/01/1963 đến 31/12/2005")]
+    [DataType(DataType.Date)]
+    public DateTime DateOfBirth { get; set; } // Ngày sinh
+    [Required(ErrorMessage = "Điểm bắt buộc phải nhập")]
+    [RegularExpression(@"^[0-9](./[0-9])?$")]
+    [Range(0, 10, ErrorMessage = "Điểm phải từ 0 đến 10")]
+    public float? Diem { get; set; } // Điểm
+}
+
+public enum Branch
+{
+
+}
+
+public enum Gender
+{
+    Nam,
+    Nu
+}
