aa53ee2ee1bb91178a704472ac08bac8c58497c2
ThinhIT137 <tranthinh130720052@gmail.com>
2025-11-07 09:31:01 +0700

    Thêm folder Day 12_lab1

---
+++ Day 12_lab1/ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Day_12_lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day_12_lab1.ViewComponents
+{
+    public class RenderViewComponent : ViewComponent
+    {
+        private readonly List<MenuItem> menuItems;
+
+        public RenderViewComponent()
+        {
+            menuItems = new List<MenuItem>()
+            {
+                new MenuItem() {Id=1, Name="List Student", Link="Admin/Student/list" },
+                new MenuItem() {Id=2, Name="Add Student", Link="Admin/Student/add" },
+            };
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("~/Views/Shared/_leftMenu.cshtml", menuItems);
+        }
+    }
+}
